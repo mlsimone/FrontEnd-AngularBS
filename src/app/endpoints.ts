f51065f1406ab73623e1b84b2b9e31fdc94a1b5a
mlsimone <msimone4@nycap.rr.com>
2023-11-20 15:54:05 -0500

    Modified to use Azure endpoints instead of Localhost endpoints.

--- src/app/endpoints.ts
+++ src/app/endpoints.ts
@@ -3,9 +3,14 @@ export const ms_graph_endpoint: string = 'https://graph.microsoft.com/v1.0/me'; 
 
 export const protectedResources = {
   APIsimple: {
-    endpointItems: " https://localhost:7267/api/Items",           // https://dataaccessrestfulapi.azurewebsites.net/api/Items
-    endpointCategories: "https://localhost:7267/api/Categories",  // https://dataaccessrestfulapi.azurewebsites.net/api/Categories
-    endpointImages: "https://localhost:7267/api/Images",          //  https://dataaccessrestfulapi.azurewebsites.net/api/Images
+    // for azure endpoints:
+    endpointItems: "https://datastoremls.azurewebsites.net/api/Items",           
+    endpointCategories: "https://datastoremls.azurewebsites.net/api/Categories", 
+    endpointImages: "https://datastoremls.azurewebsites.net/api/Images",
+    // for localhost endpoints:
+    //endpointItems:  "https://localhost:7267/api/Items",
+    //endpointCategories: "https://localhost:7267/api/Categories",
+    //endpointImages:  "https://localhost:7267/api/Images",
     scopes: {
       read: ["api://bec404a1-7783-4501-a090-2e3f885a05ff/User.Read", "api://bec404a1-7783-4501-a090-2e3f885a05ff/User.ReadWrite"],
       write: ["api://bec404a1-7783-4501-a090-2e3f885a05ff/User.ReadWrite"]
